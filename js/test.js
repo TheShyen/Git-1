65402237e92688f9df22897744799550ac6bd63d
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-04-11 02:15:13 +0500

    2 задачка

--- js/test.js
+++ js/test.js
@@ -23,3 +23,22 @@ f();
 f();
 f();
 f();
+
+
+
+/* Числа Фибоначчи с помощью замыканий; */
+
+function getFib() {
+	let a = 1;
+	let b = 1;
+    console.log(a);
+    console.log(b);
+	return function() {
+		let c = a + b;
+        a = b;
+        b = c;
+        console.log(b);
+	};
+}
+
+let a = getFib();

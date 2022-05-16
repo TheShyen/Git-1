90e910ff5cc5bd64dc6d7011c2fcbbaf74795934
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-05-16 12:53:43 +0500

    Расковырено

--- js/testPromis.js
+++ js/testPromis.js
@@ -60,7 +60,7 @@ promise.then(console.log);
 promise.then(console.log);
 promise.then(console.log); // готово! (выведется сразу)
 
-
+// микро и макрозадачи
 setTimeout(() => alert("timeout"));
 
 Promise.resolve()
@@ -70,3 +70,82 @@ alert("code");
 // сначала "code", т.к синхронный вызов
 // потом "promise", потому что .then проходит через очередь микрозадач и выполняется синхронного кода.
 // потом "timeout" , т.к это макрозадача
+Promise.resolve().then(() => console.log(2));
+console.log(1) // 1, 2
+
+let promise1 = new Promise(function(resolve, reject) {
+    setTimeout(() => reject(new Error("Aboba")), 1000);
+});
+promise1.then(
+    result => console.log(result),
+    reject => console.log(reject) // Aboba
+)
+    .then(x => console.log("OK!")); // OK
+
+let promise2 = new Promise(function(resolve, reject) {
+    setTimeout(() => reject(new Error("Aboba")), 1000);
+});
+promise2.then(
+    result => console.log(result),
+    undefined
+)
+    .catch()
+    .catch(undefined)
+    .then(x => console.log("OK!")); // в then не попадаем
+
+
+// 
+function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+      
+    script.onload = () => callback(null, script);
+    script.onerror = () => callback(new Error(`Ошибка загрузки скрипта ${src}`));
+      
+    document.head.append(script);
+}
+// Промисификация
+function loadScript(src) {
+    return new Promise(function(resolve, reject) {
+      let script = document.createElement('script');
+      script.src = src;
+  
+      script.onload = () => resolve(script);
+      script.onerror = () => reject(new Error(`Ошибка загрузки скрипта ${src}`));
+  
+      document.head.append(script);
+    });
+}
+
+// 
+
+const prom = new Promise((resolve, reject) => {
+    throw new Error("Error");
+}).catch(result => console.log("aboba")); // aboba
+
+new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      throw new Error("Whoops!");
+    }, 1000);
+  }).catch(alert); // не обрабатывает, т.к скрытый try catch обрабатывает синхронные ошибки
+
+const prom1 = new Promise((resolve, reject) =>{
+    setTimeout(() => reject(new Error("error")));
+});
+prom1.then(null, console.log) // then сработал
+     .catch(alert);
+
+const prom2 = new Promise((resolve, reject) =>{
+    setTimeout(() => resolve("aboba"));
+});
+prom2.then(console.log).catch(alert); // then сработал
+
+const prom3 = new Promise((resolve, reject) =>{
+    setTimeout(() => reject(new Error("error")));
+});
+prom3.catch(alert).then(null,console.log); // сработал alert
+
+const prom4 = new Promise((resolve, reject) =>{
+    setTimeout(() => resolve("Ruslan"));
+});
+prom4.catch(alert).then(console.log);//сработал console.log

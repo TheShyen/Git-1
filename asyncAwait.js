27284fcd6d12f3d0d1a527a40f2060a711fa869e
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-05-24 21:30:37 +0500

    Add files via upload

--- asyncAwait.js
+++ asyncAwait.js
@@ -3,7 +3,7 @@ async function returnNum() {
     return 1;
 }
 
-returnOne().then(console.log); // 1
+returnNum().then(console.log); // 1
 
 async function aboba() {
     const promise = new Promise((resolve) => {
@@ -17,4 +17,43 @@ async function aboba() {
     return result;
 }
 
-aboba().then(console.log);
+aboba().then(console.log); // 2
+
+async function asyncFun() {
+    let result = await new Promise((resolve) => {
+        setTimeout(() => resolve("aboba"), 1000);
+    });
+    return result;
+}
+
+let test = asyncFun();
+console.log(test); // Promise {<pending>} (синхронный код)
+
+
+class Thenable {
+    constructor(num) {
+        this.num = num;
+    }
+    then(resolve, reject) {
+        setTimeout(() => resolve(this.num * 5), 1000); // (*)
+    }
+}
+
+async function f() {
+    let result = await new Thenable(10);
+    console.log(result); // 50
+}
+
+function a() {
+    console.log("aboba");
+}
+
+f();
+a(); // это первее
+
+
+/* function b() {
+    let result = await new Thenable(10);
+    console.log(result);
+}
+b(); //  SyntaxError */
--- generatorTest.js
+++ generatorTest.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 function* iter() {
     yield 1;
     yield 2;
-    yield {aboba: 3};
+    yield { aboba: 3 };
     yield 4;
 }
 const arr = [];
@@ -12,8 +12,6 @@ for (let key of iter()) {
 }
 console.log(arr); // [1, 2, {aboba: 3}, 4]
 
-
-
 const test = iter();
 console.log(test.next()); // {value: 1, done: false}
 console.log(test.next()); // {value: 2, done: false}
@@ -21,20 +19,40 @@ console.log(test.next()); // {value: {…}, done: false}
 console.log(test.next()); // {value: 4, done: false}
 console.log(test.next()); // {value: undefined, done: true}
 
-
-
 const test1 = iter();
 console.log([...test1]); // [1, 2, {…}, 4]
 
 const test3 = iter();
 
-for(let key of test3) {
+for (let key of test3) {
     console.log(key); // 1
     break;
 }
 
 console.log(test3.next().value); // undefined
 
-for(let key of test3) {
+for (let key of test3) {
     console.log(key); // Генератор нельзя повторно обойти
 }
+
+// Композиция
+function* generateSequence(start, end) {
+    for (let i = start; i <= end; i++) {
+        yield i;
+    }
+}
+function* generatePasswordCodes() {
+    yield* generateSequence(48, 57);
+
+    yield* generateSequence(65, 90);
+
+    yield* generateSequence(97, 122);
+}
+
+let str = "";
+
+for (let code of generatePasswordCodes()) {
+    str += String.fromCharCode(code);
+}
+
+alert(str);
